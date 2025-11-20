5f47bdd3d4187dbadf13d85ef65016ea67f5c9a9
Adrimrtz16 <4281885@alu.murciaeduca.es>
2025-11-20 11:50:36 +0100

    api key

--- src/enrich_googlebooks.py
+++ src/enrich_googlebooks.py
@@ -1,1 +1,10 @@
-AIzaSyBbYZyGb1IgYCXUOhi9N1o8m62aOPjganE
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+GOOGLE_BOOKS_API_KEY = os.getenv("GOOGLE_BOOKS_API_KEY")
+
+if not GOOGLE_BOOKS_API_KEY:
+    raise ValueError("La variable de entorno GOOGLE_BOOKS_API_KEY no está configurada.")
+
+print(f"Tu API key es: {GOOGLE_BOOKS_API_KEY}")
--- src/scrape_goodreads.py
+++ src/scrape_goodreads.py
@@ -1,8 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
-import time
 import json
-import random
 import re
 
 HEADERS = {
